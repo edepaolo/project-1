a96bf906a5dc66f7ecd3ac31136ca36615e85045
gundrm <rachelm.gundy@gmail.com>
2019-07-13 12:53:38 -0700

    food categories added

--- predict.js
+++ predict.js
@@ -3,6 +3,25 @@ var myWolframAppId = 'PLUY24-GJAY2APWLH';
 
 var app = new Clarifai.App({apiKey: myClarifaiApiKey});
 
+// VARIABLES:
+// =======================================================================
+
+// FOOD CATEGORIES
+var carb = ["bread", "bun", "pasta"];
+var protien = ["steak", "fish", "rib eye", "clam", "lobster"];
+var dairy = ["milk", "cheese"];
+var fruitVeg = ["tomato", "potato", "orange", "strawberry", "banana", "peach"];
+var fatSugar = ["hamburger", "hot dog", "french fries"];
+
+//SWTICHING TO: index of
+//must use parimeter of tag
+function foodCategory () {
+    var tag = [];
+    var foodChoices = ["carb", "protien", 'dairy', 'fruitVeg', 'fatSugar']; 
+    document.write(array.indexOf(tag))
+    console.log('fatSugar', foodCategory());
+}
+
 /*
   Purpose: Pass information to other helper functions after a user clicks 'Predict'
   Args:
@@ -15,7 +34,7 @@ function predict_click(value, source) {
   var loader  = "https://s3.amazonaws.com/static.mlh.io/icons/loading.svg"; //spinning circle, added visual while waiting
   var reader  = new FileReader();
 
-  // load local file picture
+  // load local file picture -- can remove?
   reader.addEventListener("load", function () {
     preview.attr('style', 'background-image: url("' + reader.result + '");');
     doPredict({ base64: reader.result.split("base64,")[1] }); //base64=pulling in bianary object and keeping intact (image), reader=reads the blob(image)
@@ -24,7 +43,7 @@ function predict_click(value, source) {
   if (file) {
     reader.readAsDataURL(file);
     $('#concepts').html('<img src="' + loader + '" class="loading" />');
-  } else { alert("No file selcted!"); }
+} else { alert("No file selcted!"); }
 }
 
 /*
@@ -33,17 +52,27 @@ function predict_click(value, source) {
     value - Either {url : urlValue} or { base64 : base64Value }
 */
 function doPredict(value) {
-  app.models.predict(Clarifai.FOOD_MODEL, value).then(function(response) {
-      if(response.rawData.outputs[0].data.hasOwnProperty("concepts")) {
+app.models.predict(Clarifai.FOOD_MODEL, value).then(function(response) {
+    if(response.rawData.outputs[0].data.hasOwnProperty("concepts")) {
         var tag = response.rawData.outputs[0].data.concepts[0].name;  // clariai will determine pic is vegetable
        // var url = 'http://api.wolframalpha.com/v2/query?input='+tag+'%20nutrition%20facts&appid='+myWolframAppId; //tag = vegetable
+    console.log(tag);
+    foodCategory();
+    console.log(foodCategory());
 
-console.log(tag);
 
+
+
+//dont need because we ware not getting nutritional info
         getNutritionalInfo(url, function (result) {
-          $('#concepts').html('<h3>'+ tag + '</h3>' + "<img src='"+result+"'>");
+        $('#concepts').html('<h3>'+ tag + '</h3>' + "<img src='"+result+"'>");
         });
-      }
+
+//PUT STATEMENT TO COMPARE 'TAG' to food group
+
+
+
+    }
     }, function(err) { console.log(err); }
-  );
+);
 }
